f023d86b237fe511a6822855f4954f1c22e2c69b
Nutchanon Phongoen <nutchanon.pho@gmail.com>
2017-12-09 23:22:13 +0700

    Fix prev arrow bug

--- blockchain-academy-ssr/components/PrevArrow/index.js
+++ blockchain-academy-ssr/components/PrevArrow/index.js
@@ -4,9 +4,9 @@ export default (props) => {
   const { className, style, onClick } = props;
   return (
     <div
-      className="slick-next"
+      className="slick-prev"
       style={{
-        content: 'none', height: '38px', left: '10%', backgroundImage: 'url(/static/left-arrow.svg)',
+        zIndex: '2', height: '38px', left: '10%', backgroundImage: 'url(/static/left-arrow.svg)',
       }}
       onClick={onClick}
     />
